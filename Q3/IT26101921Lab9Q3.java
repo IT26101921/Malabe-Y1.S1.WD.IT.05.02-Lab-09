b9404920242c495da6d213eea7923fa4ed03584e
IT26101921 <it26101921@my.sliit.lk>
2026-09-23 11:37:49 +0530

    Add files via upload

---
+++ Q3/IT26101921Lab9Q3.java
@@ -0,0 +1,24 @@
+public class IT26101921Lab9Q3 {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+
+        int result1 = square(add(multiply(3, 4), multiply(5, 7)));
+
+        int result2 = add(square(add(4, 7)), square(add(8, 3)));
+
+        System.out.println("Result of (3 * 4 + 5 * 7)^2 = " + result1);
+        System.out.println("Result of (4 + 7)^2 + (8 + 3)^2 = " + result2);
+    }
+}
