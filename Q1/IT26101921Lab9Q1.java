29887f9f814e398bd6372657fe677dd3d7536018
IT26101921 <it26101921@my.sliit.lk>
2026-09-24 13:38:42 +0530

    Add files via upload

---
+++ Q1/IT26101921Lab9Q1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class IT26101921Lab9Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter value a: ");
+        double a = scanner.nextDouble();
+        
+        System.out.print("Enter value b: ");
+        double b = scanner.nextDouble();
+        
+        System.out.print("Enter value c: ");
+        double c = scanner.nextDouble();
+        
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        
+        System.out.println();
+        System.out.println("Roots are real and different :");
+        
+        double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        
+        System.out.printf("Root 1: %.2f%n", root1);
+        System.out.printf("Root 2: %.2f%n", root2);
+        
+        scanner.close();
+    }
+}
